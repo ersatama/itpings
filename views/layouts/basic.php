2030dab26a3eb491b6008a481cb165d95182d75f
= <=>
2018-05-14 14:32:10 +0600

    stage 3

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -45,11 +45,78 @@ MainAsset::register($this);
 			<div class="menu-header-in-center">
 				<div class="menu-header-in-center-list">
 					
-					<button class="menu-header-in-center-list-btn">videos<div class="menu-header-in-center-list-btn-icon"></div>
-						<div class="menu-list"></div>
+					<button class="menu-header-in-center-list-btn">Articles<div class="menu-header-in-center-list-btn-icon"></div>
+						<div class="menu-list">
+							<div class="menu-list-in">
+								<div class="menu-list-in-main">
+									<div class="menu-list-in-main-item">
+										<div class="menu-list-in-main-item-title">OS</div>
+										<div class="menu-list-in-main-item-option">Linux</div>
+										<div class="menu-list-in-main-item-option">BSD</div>
+										<div class="menu-list-in-main-item-option">Unix</div>
+									</div>
+									<div class="menu-list-in-main-item">
+										<div class="menu-list-in-main-item-title">Programming</div>
+										<div class="menu-list-in-main-item-option">Python</div>
+										<div class="menu-list-in-main-item-option">PHP</div>
+										<div class="menu-list-in-main-item-option">MySQL</div>
+										<div class="menu-list-in-main-item-option">Tools</div>
+									</div>
+									<div class="menu-list-in-main-item">
+										<div class="menu-list-in-main-item-title">Networking</div>
+										<div class="menu-list-in-main-item-option">Cisco</div>
+										<div class="menu-list-in-main-item-option">IDS</div>
+										<div class="menu-list-in-main-item-option">LAN</div>
+										<div class="menu-list-in-main-item-option">VPN</div>
+										<div class="menu-list-in-main-item-option">WAN</div>
+									</div>
+									<div class="menu-list-in-main-item">
+										<div class="menu-list-in-main-item-title">Hardware</div>
+										<div class="menu-list-in-main-item-option">Configuration</div>
+										<div class="menu-list-in-main-item-option">Development</div>
+										<div class="menu-list-in-main-item-option">Secuirty</div>
+										<div class="menu-list-in-main-item-option">Troubleshooting</div>
+										<div class="menu-list-in-main-item-option">Usage</div>
+									</div>
+									<div class="menu-list-in-main-item">
+										<div class="menu-list-in-main-item-title">Other</div>
+										<div class="menu-list-in-main-item-option">Gaming</div>
+										<div class="menu-list-in-main-item-option">Multimedia</div>	
+									</div>
+								</div>
+							</div>
+						</div>
 					</button>
-					<button class="menu-header-in-center-list-btn">Online cources<div class="menu-header-in-center-list-btn-icon"></div></button>
-					<button class="menu-header-in-center-list-btn">Articles</button>
+					<button class="menu-header-in-center-list-btn">Online cources<div class="menu-header-in-center-list-btn-icon"></div>
+						<div class="menu-list">
+							<div class="menu-list-in menu-list-in-last">
+								<div class="menu-list-in-main">
+									<div class="menu-list-in-main-item menu-list-in-main-item-last">
+										<div class="menu-list-in-main-item-title">Linux</div>
+										<div class="menu-list-in-main-item-option">Linux Zero To Hero Project</div>
+										<div class="menu-list-in-main-item-option">Secuirty</div>
+										<div class="menu-list-in-main-item-option">Web</div>
+									</div>
+									<div class="menu-list-in-main-item menu-list-in-main-item-last">
+										<div class="menu-list-in-main-item-title">Troubleshooting</div>
+										<div class="menu-list-in-main-item-option">Linux Troubleshooting</div>
+										<div class="menu-list-in-main-item-option">PHP</div>
+										<div class="menu-list-in-main-item-option">MySQL</div>
+										<div class="menu-list-in-main-item-option">Tools</div>
+									</div>
+									<div class="menu-list-in-main-item menu-list-in-main-item-last">
+										<div class="menu-list-in-main-item-title">Networking</div>
+										<div class="menu-list-in-main-item-option">Cisco</div>
+										<div class="menu-list-in-main-item-option">IDS</div>
+										<div class="menu-list-in-main-item-option">LAN</div>
+										<div class="menu-list-in-main-item-option">VPN</div>
+										<div class="menu-list-in-main-item-option">WAN</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</button>
+					<button class="menu-header-in-center-list-btn">Videos<div class="menu-header-in-center-list-btn-icon"></div></button>
 					<button class="menu-header-in-center-list-btn">About</button>
 				</div>
 			</div>
